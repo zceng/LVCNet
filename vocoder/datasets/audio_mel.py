change the calculation of mel-spectrum, modify BUG

vocoder/datasets/audio_mel.py
@@ -3,14 +3,15 @@ import numpy as np
 from scipy.io import wavfile
 
 import torch
-import torchaudio 
 from torch.utils.data import DataLoader, Dataset  
+
+from vocoder.audio import load_wav_to_torch 
 from .utils import read_metadata 
 
 class PWGAudioMelNoiseDataset(Dataset):
     ''' the Pytorch Dataset for loading audio(.wav) and mel(.npy) '''
 
-    def __init__(self, metadata_file, batch_mel_length, hop_length, cut=True):
+    def __init__(self, metadata_file, batch_mel_length, sample_rate, hop_length, cut=True):
         '''Initialize   
         Args:
             metadata_file (str): the file including paths of audio and mel.  
@@ -26,6 +27,7 @@ class PWGAudioMelNoiseDataset(Dataset):
         super().__init__() 
         self.batch_mel_length = batch_mel_length 
         self.hop_length = hop_length
+        self.sample_rate = sample_rate 
         self.cut = cut
 
         self.metadata = read_metadata( metadata_file ) 
@@ -49,7 +51,9 @@ class PWGAudioMelNoiseDataset(Dataset):
         '''
         wav_path, mel_path = self.metadata[ idx ] 
         
-        audio = torchaudio.load( wav_path )[0]
+        audio, sr = load_wav_to_torch( wav_path, self.sample_rate )
+        assert sr == self.sample_rate 
+
         mel = np.load( mel_path ) 
 
         if self.cut:

vocoder/preprocess.py
@@ -4,28 +4,29 @@ from functools import partial
 from concurrent.futures import ProcessPoolExecutor
 import numpy as np
 import torch
-import torchaudio
-from torchaudio.transforms import MelSpectrogram
+
+from .audio import TacotronSTFT, load_wav_to_torch 
 
 from vocoder.datasets.utils import save_metadata
 from vocoder.hparams import Hyperparameter 
 
+
+
 def mel_transform(wav_files, mel_dir, mel_config, device, min_wav_length):
-    device = torch.device( device )
-    transfomer = MelSpectrogram( **mel_config ).to( device ) 
+    # device = torch.device( device )
+    # transfomer = MelSpectrogram( **mel_config ).to( device ) 
+    taco_stft = TacotronSTFT( **mel_config )
     files = []
     with torch.no_grad():
         for fn in wav_files:
-            audio, sr = torchaudio.load( fn ) 
-            if sr != mel_config['sample_rate'] or audio.shape[1] < min_wav_length:
+            audio, sr = load_wav_to_torch( fn, mel_config['sampling_rate'] ) 
+            if audio.shape[1] < min_wav_length:
                 print( 'skip {}, sr: {}, length: {}'.format(fn, sr, audio.shape[1]) )
                 continue
-            mel = transfomer( audio.to(device) ) 
-            mel = 20 * torch.log10(torch.clamp(mel, min=1e-5)) - 20
-            mel = torch.clamp((mel + 100) / 100, 0.0, 1.0) 
+            # audio = audio.to( device ) 
+            mel, _ = taco_stft.mel_spectrogram( audio )
             mel_fn = os.path.join( mel_dir, os.path.basename(fn) + '.mel.npy' )
             np.save( mel_fn, mel[0].cpu().numpy().T )
-
             files.append( (fn, mel_fn) )
     return files
 
@@ -49,15 +50,13 @@ def preprocess( data_dir,
     mel_dir = os.path.join( temp_dir, 'mels' ) 
     os.makedirs(mel_dir, exist_ok=True)
     mel_config = {
-        'sample_rate': hparams.sample_rate,
+        'sampling_rate': hparams.sample_rate,
         'win_length': hparams.win_length,
         'hop_length': hparams.hop_length,
-        'n_fft': hparams.n_fft,
-        'f_min': hparams.mel_f_min,
-        'f_max': hparams.mel_f_max,
-        'n_mels': hparams.n_mels,
-        'power': 1.0,
-        'normalized': True,
+        'filter_length': hparams.n_fft,
+        'mel_fmin': hparams.mel_fmin,
+        'mel_fmax': hparams.mel_fmax,
+        'n_mel_channels': hparams.n_mels,
     }
     min_wav_length = hparams.batch_mel_length * hparams.hop_length
 

vocoder/test.py
@@ -31,8 +31,10 @@ class Tester:
 
         self.train_results = defaultdict(float) 
 
-    def restore_checkpoint(self):
-        checkpoint = os.readlink( os.path.join( self.exp_dir, 'checkpoint.pt') )
+    def restore_checkpoint(self, checkpoint=None):
+        pt = os.path.join( self.exp_dir, 'checkpoint.pt') 
+        if checkpoint is None and os.path.islink(pt):
+            checkpoint = os.path.join( self.exp_dir, os.readlink(pt) )
         state_dict = torch.load( checkpoint, map_location='cpu')  
         self.step = state_dict['step']
         self.model.load_state_dict( state_dict['model'] ) 
@@ -43,6 +45,7 @@ class Tester:
         dataset_config = {
             'metadata_file': self.hparams.test_metadata_file,
             'hop_length': self.hparams.hop_length,
+            'sample_rate': self.hparams.sample_rate, 
             'batch_mel_length': self.hparams.batch_mel_length,
             'cut': False
         }
@@ -87,6 +90,8 @@ def main():
     parser.add_argument("--exp-dir", type=str, required=True,
                         help="the directory saving expriment data, "
                         "including model checkpoints, log, results. ")
+    parser.add_argument("--checkpoint", default=None, type=str,
+                        help="checkpoint file path to load saving model")
     parser.add_argument("--device", default='cuda', type=str,
                         help="the device for training. (default: cuda:0)")
     args = parser.parse_args() 

vocoder/train.py
@@ -36,13 +36,14 @@ class Trainer:
         self.restore_checkpoint(args.restart, args.checkpoint)
 
         self.train_results = defaultdict(float) 
+        self.num_train_reuslts = 0
 
     def restore_checkpoint(self, restart=False, checkpoint=None):
         if not restart:
             try:
                 pt = os.path.join( self.exp_dir, 'checkpoint.pt') 
                 if checkpoint is None and os.path.islink(pt):
-                    checkpoint = os.readlink(pt)
+                    checkpoint = os.path.join( self.exp_dir, os.readlink(pt) )
                 if not os.path.isfile( checkpoint ):
                     print('start new training.')
                     return
@@ -67,7 +68,7 @@ class Trainer:
         torch.save( state_dict, save_path )
         if os.path.islink(link_path):
             os.unlink(link_path)
-        os.symlink(save_path, link_path)
+        os.symlink(f'checkpoint-{self.step}.pt', link_path)
         self.log.info( f'Save chechpoint as {save_path}' )
 
     def init_dataloader(self):
@@ -75,11 +76,13 @@ class Trainer:
         train_dataset_config = {
             'metadata_file': self.hparams.train_metadata_file,
             'hop_length': self.hparams.hop_length,
+            'sample_rate': self.hparams.sample_rate,
             'batch_mel_length': self.hparams.batch_mel_length
         }
         eval_dataset_config = {
             'metadata_file': self.hparams.eval_metadata_file,
             'hop_length': self.hparams.hop_length,
+            'sample_rate': self.hparams.sample_rate,
             'batch_mel_length': self.hparams.batch_mel_length
         }
         self.dataloader = {
@@ -141,13 +144,15 @@ class Trainer:
     def _check_log(self, train_result):
         for k in train_result:
             self.train_results[k] += train_result[k]
+        self.num_train_reuslts += 1
 
         if self.step % self.hparams.log_interval_steps == 0:
             for k in self.train_results:
-                v = self.train_results[k] / self.hparams.log_interval_steps 
+                v = self.train_results[k] / self.num_train_reuslts
                 self.log.add_scalar( f'train/{k}', v, self.step) 
 
             self.train_results = defaultdict(float)
+            self.num_train_reuslts = 0
         
         if self.step % self.hparams.save_interval_steps == 0:
             self.save_checkpoint()